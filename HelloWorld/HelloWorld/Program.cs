2ce2a5645054d42733725d6367e5724c2b292313
Svante-J-TE <svante.jonsson@elev.ga.ntig.se>
2020-08-30 15:10:08 +0200

    Created small form

--- HelloWorld/HelloWorld/Program.cs
+++ HelloWorld/HelloWorld/Program.cs
@@ -6,7 +6,34 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello Niklas!");
+            Console.WriteLine("Your name:");
+            string name = Console.ReadLine();
+            Console.WriteLine("Your age:");
+            int age = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Are you alive? (Answer with a \"yes\" if this is true");
+            string alive = Console.ReadLine();
+            bool lifeStatus;
+
+            if(alive.ToUpper() == "YES")
+            {
+                lifeStatus = true;
+            }
+            else
+            {
+                lifeStatus = false;
+            }
+
+            if(lifeStatus == true)
+            {
+                alive = "Alive";
+            }
+            else
+            {
+                alive = "Deceased (probably)";
+            }
+
+            Console.WriteLine('\n' + "Name: " + name + '\n' + "Age: " + age + '\n' + "Life Status: " + alive);
+
         }
     }
 }
