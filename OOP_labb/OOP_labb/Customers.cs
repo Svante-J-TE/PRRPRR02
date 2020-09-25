33cbc5f2810192c6417f33cc338f0607b60f68df
Antonkhl <54796806+Antonkhl@users.noreply.github.com>
2020-09-25 15:31:07 +0200

    oop labb

---
+++ OOP_labb/OOP_labb/Customers.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP_labb
+{
+    class Customers
+    {
+        private string _userName;
+        private int _userAge;
+        private string _adress;
+        private string _county;
+        private string _zipCode;
+        private string _email;
+        private string _phoneNumber;
+        private int[] _cart;
+    }
+}
