5064c3019a0f27c0e0184a0793706acf59eeba45
Svante-J-TE <svante.jonsson@elev.ga.ntig.se>
2020-10-09 15:12:28 +0200

    polymorfi labb

---
+++ Polymorfi/Polymorfi/Coffee.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Polymorfi
+{
+    class Coffee : Product
+    {
+        private string _roastLevel;
+
+        public string RoastLevel
+        {
+            get { return _roastLevel; }
+            set { _roastLevel = value; }
+        }
+
+    }
+}
---
+++ Polymorfi/Polymorfi/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Polymorfi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Product> cart = new List<Product>();
+
+            Milk milk = new Milk();
+            milk.FatContent = 1;
+            milk.ProductNumber = 123;
+            milk.StockStatus = 10;
+
+            cart.Add(milk);
+
+            EnergyDrink nocco = new EnergyDrink();
+            nocco.Flavor = "Cola";
+            nocco.ProductNumber = 456;
+            nocco.StockStatus = 14;
+
+            cart.Add(nocco);
+
+            Coffee coffee = new Coffee();
+            coffee.RoastLevel = "Vienna";
+            coffee.ProductNumber = 789;
+            coffee.StockStatus = 12;
+
+            cart.Add(coffee);
+
+            foreach (var product in cart)
+            {
+                if (product.GetType() == typeof(Milk))
+                {
+                    Console.WriteLine(((Milk)product).FatContent);
+                }
+                else if (product.GetType() == typeof(EnergyDrink))
+                {
+                    Console.WriteLine(((EnergyDrink)product).Flavor);
+                }
+                else if (product.GetType() == typeof(Coffee))
+                {
+                    Console.WriteLine(((Coffee)product).RoastLevel);
+                }
+            }
+        }
+    }
+}
