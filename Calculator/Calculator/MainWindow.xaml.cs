e78295d76d12b026fd413134dc301e98069bf1de
Svante-J-TE <svante.jonsson@elev.ga.ntig.se>
2020-11-16 12:05:02 +0100

    created 2darray for button structure

--- Calculator/Calculator/MainWindow.xaml.cs
+++ Calculator/Calculator/MainWindow.xaml.cs
@@ -21,27 +21,65 @@ namespace Calculator
     /// </summary>
     public partial class MainWindow : Window
     {
+        
+
         public MainWindow()
         {
+            var expressionModel = new ExpressionModel();
+
+            expressionModel.MyProperty = 1;
+
             TextBlock screen = new TextBlock
             {
                 Text = "hej hej detta är ett test",
                 FontSize = 100
             };
-            
 
             InitializeComponent();
+
+            for (int i = 0; i < 6; i++)
+            {
+                mainGrid.RowDefinitions.Add(new RowDefinition());
+            }
             for (int i = 0; i < 5; i++)
             {
                 mainGrid.ColumnDefinitions.Add(new ColumnDefinition());
-                mainGrid.RowDefinitions.Add(new RowDefinition());
+
             }
             mainGrid.RowDefinitions[0].Height = new GridLength(2.0, GridUnitType.Star);
             mainGrid.Children.Add(screen);
             screen.SetValue(Grid.RowProperty, 0);
             screen.SetValue(Grid.ColumnProperty, 0);
             screen.SetValue(Grid.ColumnSpanProperty, 5);
+
+
             
+
+            var button = new Button();
+            button.Content = "1";
+
+            string[,] buttonContent = new string[,]
+            {
+                {"^x", "√", "(", ")", "Quit"},
+                {"7", "8", "9", "DEL", "AC"},
+                {"4", "5", "6", "X", "/"},
+                {"1", "2", "3", "+", "-"},
+                {"0", ".", "EXP", "(-)", "EXE"}
+            };
+
+            for (int i = 0; i < buttonContent.GetLength(0); i++)
+            {
+                for (int j = 0; j < buttonContent.GetLength(1); j++)
+                {
+                    Button btn = new Button { 
+                        Content = buttonContent[i, j],
+                    };
+                    btn.Click += Button_Clicked();
+                    mainGrid.Children.Add(btn)
+                }
+            }
+
+            /*
             
             for (int i = 0; i < 12; i++)
             {
@@ -81,8 +119,13 @@ namespace Calculator
                     btn.SetValue(Grid.RowProperty, 3 + i/3);
                     btn.SetValue(Grid.ColumnProperty, 3 + i%3);
                 }*/
-
+            
             }
+
+        private RoutedEventHandler Button_Clicked()
+        {
+            throw new NotImplementedException();
         }
+    }
     }
 }
