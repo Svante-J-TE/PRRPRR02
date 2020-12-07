f889e7f3c4d6fd9131a4f2883b85c0bcea7057bb
Svante-J-TE <54800689+Svante-J-TE@users.noreply.github.com>
2020-12-07 12:28:06 +0100

    abstraction demo

---
+++ Abstraktion/Abstraktion/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Abstraktion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Animal> animals = new List<Animal>();
+
+            var dog = new Dog
+            {
+                Color = "Black"
+            };
+
+            var shark = new Shark
+            {
+                Color = "Grey"
+            };
+
+            animals.AddRange(new Animal[] { dog, shark });
+
+            foreach (var animal in animals)
+            {
+                animal.Breathe();
+                animal.Eat();
+                animal.Rest();
+                Console.WriteLine("The animal is " + animal.Color);
+
+                if(animal is Shark tempShark)
+                {
+                    tempShark.Swim();
+                }
+            }
+        }
+    }
+}
---
+++ Abstraktion/Abstraktion/Shark.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Abstraktion
+{
+    class Shark : Animal
+    {
+        public Shark()
+        {
+            NumberOfLimbs = 4;
+        }
+        public override void Breathe()
+        {
+            Console.WriteLine("Shark breathes using its gills!");
+        }
+
+        public override void Eat()
+        {
+            Console.WriteLine("Shark is chomping!");
+        }
+
+        public override void Rest()
+        {
+            Console.WriteLine("Shark is in an restful state");
+        }
+
+        //
+        public void Swim()
+        {
+            Console.WriteLine("Shark is swimming!");
+        }
+    }
+}
